d3ec8d6a9c50a7f721f4ee73e80d0d63e53dadca
Robert Dunn <rdskifreak@gmail.com>
2017-09-07 23:26:22 -0600

    completed asg 2 q 2.

---
+++ a2q2.m
@@ -0,0 +1,11 @@
+syms n
+f = @(x,y) symsum(sin(x^n)+3*y^n,n,1,5);
+x = linspace(-1,1,400);
+y = 0.5;
+z = zeros(1,400);
+for i=1:1:400
+   z(i) = f(x(i),y);
+end
+fplot(x,z)
+xlabel('x');
+ylabel('z');
