d0c3611f2f1a1b4d4b36566e44112b63e58c32ab
Robert Dunn <donald.dunn@ucalgary.ca>
2017-09-13 15:58:52 -0600

    compleded asg 2 q 4 I Think.

--- a2q4_frequency_responce.m
+++ a2q4_frequency_responce.m
@@ -1,3 +1,7 @@
-function [f] = a2q4_frequency_responce(w)
-    f = abs(10./(10 + w + 1./(w)));
+function [freq_responce] = a2q4_frequency_responce()
+    w1 = linspace(0,10);
+    freq_responce = abs(10./(10 + w1 + 1./(w1)));
+    w2 = linspace(0.01,100);
+    f = abs(10./(10 + w2 + 1./(w2)));
+    semilogx(w2,f);
 end
