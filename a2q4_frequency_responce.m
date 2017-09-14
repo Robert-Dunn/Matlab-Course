842863f366edb44710916f0d2ada76e807bc1ead
Robert Dunn <rdskifreak@gmail.com>
2017-09-13 22:17:21 -0600

    added j to q 3 and 4 of asg 2

--- a2q4_frequency_responce.m
+++ a2q4_frequency_responce.m
@@ -1,7 +1,8 @@
 function [freq_responce] = a2q4_frequency_responce()
     w1 = linspace(0,10);
-    freq_responce = abs(10./(10 + w1 + 1./(w1)));
+    freq_responce = abs(10./(10 + j.*w1 + 1./(j.*w1)));
     w2 = linspace(0.01,100);
-    f = abs(10./(10 + w2 + 1./(w2)));
+    f = abs(10./(10 + j.*w2 + 1./(j.*w2)));
     semilogx(w2,f);
 end
+
