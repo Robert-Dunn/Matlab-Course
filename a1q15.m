608b3f45fc957b7e2e6bb079c091f79efecaef92
Robert Dunn <donald.dunn@ucalgary.ca>
2017-09-22 19:43:40 -0600

    fixed question 15 assignment 1 by adding syms t.

--- a1q15.m
+++ a1q15.m
@@ -1,8 +1,10 @@
-x = 3*t^1.3/(1+t^3);
+syms t;
 y = 3*t^2/(1+t^3);
+x = 3*t^1.3/(1+t^3);
+subplot(2,1,1);
 ezplot(x,[0,10]);
 hold on
-ezplot(y,[0,10]);
+ezplot(y,[0,10])
 hold off
 legend('x','y')
 subplot(2,1,2);
