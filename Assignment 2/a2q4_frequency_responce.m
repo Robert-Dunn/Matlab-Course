343f30253367a0d2291c5c0af51d8d249b6d4b24
Robert Dunn <donald.dunn@ucalgary.ca>
2017-10-01 17:38:00 -0600

    finished asg 2 q 5.

--- Assignment 2/a2q4_frequency_responce.m
+++ Assignment 2/a2q4_frequency_responce.m
@@ -3,4 +3,11 @@ function [freq] = a2q4_frequency_responce(w)
 freq=abs(10./(10+1i.*w+(1./(1i.*w))));
 end	
 %In Command Line:
-
+w1 = linspace(0,10,100);
+w2 = linspace(0.01,100,100);
+frequency1 = a2q4_frequency_responce(w1);
+frequency2= a2q4_frequency_responce(w2);
+subplot(2,1,1)
+plot(w1,frequency1);
+subplot(2,1,2)
+semilogx(w2,frequency2);
--- Assignment 2/a2q5_phase.m
+++ Assignment 2/a2q5_phase.m
@@ -1,6 +1,11 @@
+%in function
 function [phase] = a2q5_phase(R,L,C,w)
     H = (R./(R+1i.*w.*L+(1./(1i.*w.*C))));
-    R = real(H);
-    I = imag(H);
-    phase=rad2deg(atan(I./R));
+    r = real(H);
+    im = imag(H);
+    phase=rad2deg(atan(im./r));
 end
+%in command line
+w = linspace(.01, 100, 1000);
+semilogx(w, a2q5_phase(1, 1, 1, w))
+grid on
